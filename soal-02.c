5e43e273485e308fe3b4c5a648b13953e78e8609
dismaswidyanto <63232560+dismaswidyanto@users.noreply.github.com>
2022-03-13 19:02:30 +0700

    Update soal-02.c

--- soal-02.c
+++ soal-02.c
@@ -17,12 +17,71 @@ typedef struct complex {
 } complex;
 
 int main() {
-	//insert code here 
 
-	//template output :
+	int baris, kolom;
+	scanf("%d %d",&baris, &kolom);
 
-	printf("Matriks merupakan matriks skew hermitian dan matriks hermitian\n");
-	printf("Matriks merupakan matriks hermitian\n");
-	printf("Matriks merupakan matriks skew hermitian\n");
-	printf("Matriks bukan merupakan keduanya\n");
+	int is_hermitian=1;
+	int is_skew_hermitian=1;
+
+	// jika baris tidak sama dengan kolom, sudah pasti bukan matriks hermitian
+	if(baris!=kolom) {
+		printf("Matriks bukan merupakan keduanya\n");
+	}
+	else {
+		//deklarasi matrices of struct
+		complex matriks[baris][kolom];
+		complex conjugate_transpose[kolom][baris];
+
+		for (int i=0;i<baris;i++) {
+			for(int j=0;j<baris;j++) {
+				scanf("%d %d", &matriks[i][j].real, &matriks[i][j].imag);
+			}
+		}
+		//melakukan conjugate transpose matriks
+		for (int i=0; i<baris;i++) {
+			for(int j=0;j<kolom;j++) {
+				conjugate_transpose[j][i].real=matriks[i][j].real;
+				conjugate_transpose[j][i].imag=(-1)*matriks[i][j].imag;
+			}
+		}
+		for(int i=0;i<baris;i++) {
+
+			for(int j=0;j<kolom;j++) {
+				if(is_hermitian==1) { //cek apakah suatu matriks masih hermitian
+					if(conjugate_transpose[i][j].real!=matriks[i][j].real || conjugate_transpose[i][j].imag!=matriks[i][j].imag) {
+						is_hermitian=0;
+					}
+				}
+				if(is_skew_hermitian==1) {
+					if(conjugate_transpose[i][j].real!=-1*matriks[i][j].real || conjugate_transpose[i][j].imag!= -1*matriks[i][j].imag) {
+						is_skew_hermitian=0;
+					}
+				}
+				if(is_hermitian==0 && is_skew_hermitian==0) {
+					printf("Matriks bukan merupakan keduanya\n");
+					return 0;
+				}
+			}   
+		}
+		
+		//sudah menyelesaikan loop di atas, artinya semua komponen sama, matriks merupakan matriks hermitian
+		
+		if(is_hermitian==1) {
+			if(is_skew_hermitian==1) {
+				printf("Matriks merupakan matriks skew hermitian dan matriks hermitian\n");
+			}
+			else {
+				printf("Matriks merupakan matriks hermitian\n");
+			}
+		}
+		else {
+			if(is_skew_hermitian==1) {
+				printf("Matriks merupakan matriks skew hermitian\n");
+			}
+			else {
+				printf("Matriks bukan merupakan keduanya\n");
+			}
+		}
+	}
 }
